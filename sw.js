4ba59b266787cd95a72b3679ee27ab1b5fb77560
thenetworK1NG <thenetworking888@gmail.com>
2025-08-13 00:37:40 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,66 @@
+// Service Worker
+const CACHE_NAME = 'telegenix-progress-app-v1';
+const ASSETS_TO_CACHE = [
+  './SearchTask.html',
+  './manifest.json',
+  './DESIGN.png',
+  './DONE.png',
+  './manufacturing-process.png',
+  './NEW.png',
+  './PENDING PAYMENT.png',
+  './PRINTING.png',
+  './QUEUE FOR PRINTING.png'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  // For API calls
+  if (event.request.url.includes('jsonrpc.php')) {
+    event.respondWith(
+      fetch(event.request)
+        .catch(() => {
+          return caches.match(event.request);
+        })
+    );
+  } else {
+    // For static assets
+    event.respondWith(
+      caches.match(event.request)
+        .then((response) => {
+          return response || fetch(event.request)
+            .then((fetchResponse) => {
+              return caches.open(CACHE_NAME)
+                .then((cache) => {
+                  cache.put(event.request, fetchResponse.clone());
+                  return fetchResponse;
+                });
+            });
+        })
+    );
+  }
+});
